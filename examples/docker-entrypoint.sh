716da45c91ac2a498dc61562b1c697b3f3b37214
Thijs Hulshof <thijs@cloudlets.io>
2020-02-23 13:42:44 +0100

    Simplify example

--- examples/docker-entrypoint.sh
+++ examples/docker-entrypoint.sh
@@ -2,7 +2,6 @@
 
 # Create empty .env-file if it doesn't exist yet. Will be populated by app/bin/functions.sh, which will be invoked by our entrypoint.
 touch /var/www/html/shopware/.env
-
 ./app/bin/post-install.sh
 
 exec "$@"
